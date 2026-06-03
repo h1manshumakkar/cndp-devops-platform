dd0bdc1607639834dddebb3a1a8d6177ffaa9d6b
Himanshu <Himanshu.makkae@gmail.com>
2026-06-03 18:12:18 -0400

    adding AKS via Terraform

---
+++ app/Terraform/aks.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "cndp-aks-cluster"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "cndpaks"
+
+  default_node_pool {
+    name       = "nodepool1"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
