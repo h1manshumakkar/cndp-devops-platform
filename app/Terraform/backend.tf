8fe4130e2b80eed5db2b50b7835583315277d786
Himanshu <Himanshu.makkae@gmail.com>
2026-06-30 12:58:00 -0400

    rebuilding2.0

--- app/Terraform/backend.tf
+++ app/Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "cndptfstate123"
+    resource_group_name  = "terraform-backend-rg"
+    storage_account_name = "cndptfstate"
     container_name       = "tfstate"
     key                  = "cndp-devops.tfstate"
   }
